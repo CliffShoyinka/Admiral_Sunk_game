7897ea42a9853007f0ac8a4c3e3084f74b6ec36b
Mehmethan Bellek <bellekmehmethan@gmail.com>
2023-02-25 21:07:10 +0300

    March 25,2023 v2

--- src/Runner.java
+++ src/Runner.java
@@ -6,8 +6,23 @@ public class Runner {
     public static void main(String[] args) {
 
         AdmiralSunkGame game = new AdmiralSunkGame(10, 3, 10);
+        Scanner input = new Scanner(System.in);
 
-        game.play();
+        System.out.println("==================== ADMIRAL SUNK ======================");
+        System.out.println();
+
+        boolean playAgain = true;
+
+        while (playAgain) {
+            game.play();
+
+            System.out.println("Do you want to play again? (y/n)");
+            String idx = input.next();
+
+            if (!idx.equalsIgnoreCase("y")) {
+                playAgain = false;
+            }
+        }
 
     }
 }
